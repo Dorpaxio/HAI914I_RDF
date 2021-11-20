81b966a1a129dbeefa8da8b4c9bd8f1b74f006c3
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-11-20 16:25:28 +0100

    data access

--- src/main/java/qengine/program/Main.java
+++ src/main/java/qengine/program/Main.java
@@ -1,17 +1,9 @@
 package qengine.program;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Stream;
-
+import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.query.algebra.Projection;
 import org.eclipse.rdf4j.query.algebra.StatementPattern;
+import org.eclipse.rdf4j.query.algebra.Var;
 import org.eclipse.rdf4j.query.algebra.helpers.AbstractQueryModelVisitor;
 import org.eclipse.rdf4j.query.algebra.helpers.StatementPatternCollector;
 import org.eclipse.rdf4j.query.parser.ParsedQuery;
@@ -26,134 +18,174 @@ import qengine.program.RDFHandlers.DictionaryRDFHandler;
 import qengine.program.RDFHandlers.IndexRDFHandler;
 import qengine.program.RDFHandlers.MainRDFHandler;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
+
 /**
  * Programme simple lisant un fichier de requête et un fichier de données.
- * 
+ *
  * <p>
  * Les entrées sont données ici de manière statique,
  * à vous de programmer les entrées par passage d'arguments en ligne de commande comme demandé dans l'énoncé.
  * </p>
- * 
+ *
  * <p>
  * Le présent programme se contente de vous montrer la voie pour lire les triples et requêtes
  * depuis les fichiers ; ce sera à vous d'adapter/réécrire le code pour finalement utiliser les requêtes et interroger les données.
  * On ne s'attend pas forcémment à ce que vous gardiez la même structure de code, vous pouvez tout réécrire.
  * </p>
- * 
+ *
  * @author Olivier Rodriguez <olivier.rodriguez1@umontpellier.fr>
  */
 final class Main {
-	static final String baseURI = null;
+    static final String baseURI = null;
 
-	/**
-	 * Votre répertoire de travail où vont se trouver les fichiers à lire
-	 */
-	static final String workingDir = "data/";
+    /**
+     * Votre répertoire de travail où vont se trouver les fichiers à lire
+     */
+    static final String workingDir = "data/";
 
-	/**
-	 * Fichier contenant les requêtes sparql
-	 */
-	static final String queryFile = workingDir + "sample_query.queryset";
+    /**
+     * Fichier contenant les requêtes sparql
+     */
+    static final String queryFile = workingDir + "STAR_ALL_workload.queryset";
 
-	/**
-	 * Fichier contenant des données rdf
-	 */
-	static final String dataFile = workingDir + "sample_data.nt";
+    /**
+     * Fichier contenant des données rdf
+     */
+    static final String dataFile = workingDir + "100K.nt";
 
-	// ========================================================================
+    // ========================================================================
 
-	/**
-	 * Méthode utilisée ici lors du parsing de requête sparql pour agir sur l'objet obtenu.
-	 */
-	public static void processAQuery(ParsedQuery query) {
-		List<StatementPattern> patterns = StatementPatternCollector.process(query.getTupleExpr());
+    /**
+     * Méthode utilisée ici lors du parsing de requête sparql pour agir sur l'objet obtenu.
+     */
+    public static void processAQuery(ParsedQuery query, Dictionary dictionary, Index index) {
+        final List<StatementPattern> patterns = StatementPatternCollector.process(query.getTupleExpr());
 
-		System.out.println("first pattern : " + patterns.get(0));
+        // {vO: [23923, 20323], v1: [2322, 2, 999]} -> Solutions possible pour chaque élément
+        final Map<String, Set<Integer>> projectionElems = new HashMap<>();
+        query.getTupleExpr().visit(new AbstractQueryModelVisitor<RuntimeException>() {
+            public void meet(Projection projection) {
+                projection.getProjectionElemList().getElements().forEach(projectionElem -> {
+                    projectionElems.computeIfAbsent(projectionElem.getSourceName(), el -> new HashSet<>());
+                });
+            }
+        });
 
-		System.out.println("object of the first pattern : " + patterns.get(0).getObjectVar().getValue());
+        for (int i = 0; i < patterns.size(); i++) {
+            final Map.Entry<String, Set<Integer>> resultMap = getResultsFromPattern(patterns.get(i), dictionary, index);
+            if (i == 0) {
+                projectionElems.get(resultMap.getKey()).addAll(resultMap.getValue());
+            } else {
+                projectionElems.get(resultMap.getKey()).retainAll(resultMap.getValue());
+            }
+        }
 
-		System.out.println("variables to project : ");
+        System.out.println(projectionElems);
 
-		// Utilisation d'une classe anonyme
-		query.getTupleExpr().visit(new AbstractQueryModelVisitor<RuntimeException>() {
+    }
 
-			public void meet(Projection projection) {
-				System.out.println(projection.getProjectionElemList().getElements());
-			}
-		});
-	}
+    private static Map.Entry<String, Set<Integer>> getResultsFromPattern(StatementPattern pattern, Dictionary dictionary, Index index) {
+        final Set<Integer> results = new HashSet<>();
+        final Var subjectVar = pattern.getSubjectVar();
+        final Var predicateVar = pattern.getPredicateVar();
+        final Var objectVar = pattern.getObjectVar();
 
-	/**
-	 * Entrée du programme
-	 */
-	public static void main(String[] args) throws Exception {
-		Dictionary dictionary = new Dictionary();
-		Index index = new Index();
+        final int subject = getIdFromValue(subjectVar.getValue(), dictionary);
+        final int predicate = getIdFromValue(predicateVar.getValue(), dictionary);
+        final int object = getIdFromValue(objectVar.getValue(), dictionary);
 
-		parseData(dictionary, index);
-		//parseQueries();
-		System.out.println(dictionary);
-		System.out.println(index);
-	}
+        final String varName;
 
-	// ========================================================================
+        Index.Order order = Index.Order.getBestOrder(subject, predicate, object);
+        results.addAll(index.getResults(subject, predicate, object, order));
+        if (subject == -1) varName = subjectVar.getName();
+        else if (predicate == -1) varName = predicateVar.getName();
+        else varName = objectVar.getName();
 
-	/**
-	 * Traite chaque requête lue dans {@link #queryFile} avec {@link #processAQuery(ParsedQuery)}.
-	 */
-	private static void parseQueries() throws FileNotFoundException, IOException {
-		/**
-		 * Try-with-resources
-		 * 
-		 * @see <a href="https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html">Try-with-resources</a>
-		 */
-		/*
-		 * On utilise un stream pour lire les lignes une par une, sans avoir à toutes les stocker
-		 * entièrement dans une collection.
-		 */
-		try (Stream<String> lineStream = Files.lines(Paths.get(queryFile))) {
-			SPARQLParser sparqlParser = new SPARQLParser();
-			Iterator<String> lineIterator = lineStream.iterator();
-			StringBuilder queryString = new StringBuilder();
+        return new AbstractMap.SimpleEntry<>(varName, results);
+    }
 
-			while (lineIterator.hasNext())
-			/*
-			 * On stocke plusieurs lignes jusqu'à ce que l'une d'entre elles se termine par un '}'
-			 * On considère alors que c'est la fin d'une requête
-			 */
-			{
-				String line = lineIterator.next();
-				queryString.append(line);
+    private static int getIdFromValue(Value value, Dictionary dictionary) {
+        if (value == null) return -1;
+        return Optional.ofNullable(dictionary.get(value.stringValue())).orElse(-1);
+    }
 
-				if (line.trim().endsWith("}")) {
-					ParsedQuery query = sparqlParser.parseQuery(queryString.toString(), baseURI);
+    /**
+     * Entrée du programme
+     */
+    public static void main(String[] args) throws Exception {
+        Dictionary dictionary = new Dictionary();
+        Index index = new Index();
 
-					processAQuery(query); // Traitement de la requête, à adapter/réécrire pour votre programme
+        parseData(dictionary, index);
+        parseQueries(dictionary, index);
+    }
 
-					queryString.setLength(0); // Reset le buffer de la requête en chaine vide
-				}
-			}
-		}
-	}
+    // ========================================================================
 
-	/**
-	 * Traite chaque triple lu dans {@link #dataFile} avec {@link MainRDFHandler}.
-	 */
-	private static void parseData(Dictionary dictionary, Index index) throws IOException {
-		parse(new DictionaryRDFHandler(dictionary));
-		parse(new IndexRDFHandler(index, dictionary));
-	}
+    /**
+     * Traite chaque requête lue dans {@link #queryFile} avec {@link #processAQuery(ParsedQuery)}.
+     */
+    private static void parseQueries(Dictionary dictionary, Index index) throws FileNotFoundException, IOException {
+        /**
+         * Try-with-resources
+         *
+         * @see <a href="https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html">Try-with-resources</a>
+         */
+        /*
+         * On utilise un stream pour lire les lignes une par une, sans avoir à toutes les stocker
+         * entièrement dans une collection.
+         */
+        try (Stream<String> lineStream = Files.lines(Paths.get(queryFile))) {
+            SPARQLParser sparqlParser = new SPARQLParser();
+            Iterator<String> lineIterator = lineStream.iterator();
+            StringBuilder queryString = new StringBuilder();
 
-	private static void parse(AbstractRDFHandler abstractRDFHandler) throws FileNotFoundException, IOException {
-		try (Reader dataReader = new FileReader(dataFile)) {
-			// On va parser des données au format ntriples
-			RDFParser rdfParser = Rio.createParser(RDFFormat.NTRIPLES);
+            while (lineIterator.hasNext())
+                /*
+                 * On stocke plusieurs lignes jusqu'à ce que l'une d'entre elles se termine par un '}'
+                 * On considère alors que c'est la fin d'une requête
+                 */ {
+                String line = lineIterator.next();
+                queryString.append(line);
 
-			// On utilise notre implémentation de handler
-			rdfParser.setRDFHandler(abstractRDFHandler);
+                if (line.trim().endsWith("}")) {
+                    ParsedQuery query = sparqlParser.parseQuery(queryString.toString(), baseURI);
 
-			// Parsing et traitement de chaque triple par le handler
-			rdfParser.parse(dataReader, baseURI);
-		}
-	}
+                    processAQuery(query, dictionary, index); // Traitement de la requête, à adapter/réécrire pour votre programme
+
+                    queryString.setLength(0); // Reset le buffer de la requête en chaine vide
+                }
+            }
+        }
+    }
+
+    /**
+     * Traite chaque triple lu dans {@link #dataFile} avec {@link MainRDFHandler}.
+     */
+    private static void parseData(Dictionary dictionary, Index index) throws IOException {
+        parse(new DictionaryRDFHandler(dictionary));
+        parse(new IndexRDFHandler(index, dictionary));
+    }
+
+    private static void parse(AbstractRDFHandler abstractRDFHandler) throws FileNotFoundException, IOException {
+        try (Reader dataReader = new FileReader(dataFile)) {
+            // On va parser des données au format ntriples
+            RDFParser rdfParser = Rio.createParser(RDFFormat.NTRIPLES);
+
+            // On utilise notre implémentation de handler
+            rdfParser.setRDFHandler(abstractRDFHandler);
+
+            // Parsing et traitement de chaque triple par le handler
+            rdfParser.parse(dataReader, baseURI);
+        }
+    }
 }
--- src/main/java/qengine/program/Models/Index.java
+++ src/main/java/qengine/program/Models/Index.java
@@ -1,21 +1,19 @@
 package qengine.program.Models;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Index {
 
     public enum Order {
-        SPO(new int[] {0, 1, 2}),
-        SOP(new int[] {0, 2, 1}),
-        PSO(new int[] {1, 0, 2}),
-        POS(new int[] {1, 2, 0}),
-        OSP(new int[] {2, 0, 1}),
-        OPS(new int[] {2, 1, 0});
+        SPO(new int[]{0, 1, 2}),
+        SOP(new int[]{0, 2, 1}),
+        PSO(new int[]{1, 0, 2}),
+        POS(new int[]{1, 2, 0}),
+        OSP(new int[]{2, 0, 1}),
+        OPS(new int[]{2, 1, 0});
 
         private final int[] order;
+
         Order(int[] order) {
             this.order = order;
         }
@@ -23,11 +21,21 @@ public class Index {
         public int[] getOrder() {
             return order;
         }
+
+        public static Order getBestOrder(int subject, int predicate, int object) {
+            subject = subject > 0 ? 1 : 0;
+            predicate = predicate > 0 ? 1 : 0;
+            object = object > 0 ? 1 : 0;
+
+            if (subject == 1 && predicate == 1 && object == 0) return SPO;
+            else if (subject == 1 && predicate == 0 && object == 1) return SOP;
+            else return POS;
+        }
     }
 
     public static class Store extends HashMap<Integer, HashMap<Integer, Set<Integer>>> {
         public void put(int a, int b, int c) {
-            HashMap<Integer, Set<Integer>> action = computeIfAbsent(a, k -> new HashMap<>());
+            Map<Integer, Set<Integer>> action = computeIfAbsent(a, k -> new HashMap<>());
             Set<Integer> objects = action.computeIfAbsent(b, k -> new HashSet<>());
             objects.add(c);
         }
@@ -45,6 +53,13 @@ public class Index {
             final int[] orderIds = order.getOrder();
             store.put(fields[orderIds[0]], fields[orderIds[1]], fields[orderIds[2]]);
         });
+    }
+
+    public Set<Integer> getResults(int subject, int predicate, int object, Order order) {
+        final int[] fields = {subject, predicate, object};
+        final int[] orderIds = order.getOrder();
+        return Optional.ofNullable(stores.get(order).get(fields[orderIds[0]]).get(fields[orderIds[1]]))
+                .orElse(new HashSet<>());
     }
 
     @Override
